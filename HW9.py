63f576c6b0322e21815fcef03d8941c433d4914b
MAZaytsev85 <streetrsr2007@yandex.ru>
2023-09-28 00:03:08 +0300

    Initial commit

---
+++ HW9.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = pd.read_csv("california_housing_train.csv")
+
+print(f"Cредняя стоимость дома, где кол-во людей от 0 до 500 - {data[(data['population'] < 500) & (data['population'] > 0)] ['median_house_value'].mean()}")
+
+print(f"Максимальная households в зоне минимального значения population - {data[data['population'] == data['population'].min()]['households'].max()}")
